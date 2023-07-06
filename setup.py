9118355e0e93c41b9e108f1d4de6bf2acdebae9d
sweeneyde <sweeney.427@osu.edu>
2023-07-06 12:30:38 -0400

    add pyproject.toml

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="multimerge",
-    version="0.1",
+    version="0.2.0",
     description="A faster multi-way merge algorithm interchangeable with heapq.merge",
     long_description=long_description,
     long_description_content_type="text/markdown",
