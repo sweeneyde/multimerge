8a3397e5ac91a091d7784537762da0c70078a277
Dennis Sweeney <36520290+sweeneyde@users.noreply.github.com>
2023-07-06 13:30:56 -0400

    Use cibuildwheel and pyproject.toml (#7)

--- setup.py
+++ setup.py
@@ -1,21 +1,10 @@
 from setuptools import setup, Extension
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
     name="multimerge",
-    version="0.1",
-    description="A faster multi-way merge algorithm interchangeable with heapq.merge",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    version="0.2.0",
     author="Dennis Sweeney",
     author_email="sweeney.427@osu.edu",
-    license="MIT",
+    description="a k-way merge algorithm to replace heapq.merge",
     ext_modules=[Extension("multimerge", ["multimergemodule.c"])],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
 )
