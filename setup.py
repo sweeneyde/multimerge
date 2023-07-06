a5d865d9b4cf411b14aecb932a30d627da6ea982
sweeneyde <sweeney.427@osu.edu>
2023-07-06 12:56:48 -0400

    simplify setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,10 @@
 from setuptools import setup, Extension
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
     name="multimerge",
     version="0.2.0",
-    description="A faster multi-way merge algorithm interchangeable with heapq.merge",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     author="Dennis Sweeney",
     author_email="sweeney.427@osu.edu",
-    license="MIT",
+    description="A faster multi-way merge algorithm interchangeable with heapq.merge",
     ext_modules=[Extension("multimerge", ["multimergemodule.c"])],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
 )
